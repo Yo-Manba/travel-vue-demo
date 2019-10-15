11540adca95267024b78444ed70755fd0790d0b6
Lambert <694010493@qq.com>
2019-10-15 21:37:47 +0800

    change icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img-box">
+                <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeIcons'
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .icons
+        height 0
+        padding-bottom 50%
+        overflow hidden
+        .icon
+            float left
+            width 25%
+            height 0
+            padding-bottom 25%
+            position relative
+            .icon-img-box
+                position absolute
+                top 0
+                left 0
+                right 0
+                bottom .44rem
+                padding 0.15rem
+                box-sizing border-box
+                .icon-img
+                    display block
+                    margin 0 auto
+                    height 100%
+            .icon-desc
+                position absolute
+                left 0
+                right 0
+                bottom 0
+                height .44rem
+                line-height .44rem
+                text-align center
+                color $dackTextColor
+</style>
